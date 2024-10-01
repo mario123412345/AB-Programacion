dd0b7f5f83783325de1fe7551cb505cac8939d48
mario123412345 <154319877+mario123412345@users.noreply.github.com>
2024-10-01 15:22:16 +0200

    Agregar archivos de proyecto.

---
+++ AB Programacion/AB Programacion.cpp
@@ -0,0 +1,12 @@
+﻿// AB Programacion.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "AB Programacion.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
