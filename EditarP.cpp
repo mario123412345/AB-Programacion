ffe120ca8b47f9f0d63428e3dbdbbe26b7b358b1
mario123412345 <154319877+mario123412345@users.noreply.github.com>
2024-12-18 19:32:40 +0100

    Se puede ver el paciente que se desea editar y seleccionar que dato se quiere editar, pero da errores en el json al editar algún dato

--- EditarP.cpp
+++ EditarP.cpp
@@ -16,22 +16,51 @@ void peditar() {
     for (auto& pacient : paci) {
         if (pacient["ID"] == edp) {
             cout << "Pacientes encontrado  con id" <<edp<< endl;
-            cout << "Nombre paciente: ";
-            cin >> pacient["nombre"];
-            cout << "Apellido 1 paciente: ";
-            cin >> pacient["apellido1"];
-            cout << "Apellido 2paciente: ";
-            cin >> pacient["apellido2"];
-            cout << "Enfermedad paciente: ";
-            cin >> pacient["enfermedad"];
-            cout << "Fecha baja paciente:(dia/mes/año)";
-            cin >> pacient["fechabaja"];
-            cout << "Fecha alta paciente:(dia/mes/año) ";
-            cin >> pacient["fechaalta"];
+            cout << "1.Nombre paciente: " << pacient["Nombre"] << endl ;
+            cout << "2.Apellido 1 paciente: "<< pacient["Apellido1"]<<endl;
+            cout << "3.Apellido 2paciente: "<< pacient["Apellido2"] << endl;
+            cout << "4.Enfermedad paciente: "<< pacient["Enfermedad"] << endl;
+            cout << "5.Fecha baja paciente:(dia/mes/año)"<< pacient["Fecha baja"] << endl;
+            cout << "6.Fecha alta paciente:(dia/mes/año) "<< pacient["Fecha alta"] << endl;
+            int elect;
+            cout << "Que opción quieres editar?";
+            cin >> elect;
+            switch (elect) {
+            case 1:
+                cout << "Cambiar nombre" << endl;
+                cin >> pacient["Nombre"];
+            break;
+            case 2:
+                cout << "Cambiar apellido1" << endl;
+                cin >> pacient["Apellido1"];
+                break;
+            case 3:
+                cout << "Cambiar apellido2" << endl;
+                cin >> pacient["Apellido2"];
+                break;
+            case 4:
+                cout << "Cambiar problema/enfermedad del paciente" << endl;
+                cin >> pacient["Enfermedad"];
+                break;
+            case 5:
+                cout << "Cambiar la fecha de baja" << endl;
+                cin >> pacient["Fecha baja"];
+                break;
+            case 6:
+                cout << "Cambiar la fecha de alta" << endl;
+                cin >> pacient["Fecha alta"];
+                break;
+            default:
+                cout << "No se puede editar algo que no existe"<<endl;
+                break;
+            }
             encontrar = true;
             break;
         }
        
+    }
+    if (!encontrar) {
+        cout << "No hay pacientes con id" << edp << endl;
     }
     if (!arch) {
         cout << "Error2" << endl;
