d5aaf0278e8beeff3afcd508597a9b513d8e8b3b
mario123412345 <154319877+mario123412345@users.noreply.github.com>
2024-12-29 12:09:55 +0100

    void de eliminar doctores relacionado a la clase

--- Eliminar Doctor.cpp
+++ Eliminar Doctor.cpp
@@ -1,5 +1,5 @@
 #include "AB Programacion.h"
-void deliminar() {
+/*void deliminar() {
     cout << "ID del doctor que quieres eliminar: ";
     int idd;
     cin >> idd;
@@ -44,10 +44,54 @@ void deliminar() {
     }
     cambioelim << doctoreselim.dump(4);
     cambioelim.close();
-
-
+}*/
+void doctor::eliminardoctor(int idd) {
+    ifstream doctoresj("doctores.json");
+    if (!doctoresj.is_open()) {
+        cout << "Archivo de doctores ya abierto" << endl;
+    }
+    json doctores;
+    doctoresj >> doctores;
+    doctoresj.close();
+    json doctoreselim;
+    ifstream doctoreselimjson("doctoreselim.json");
+    if (!doctoreselimjson.is_open()) {
+        doctoreselimjson >> doctoreselim;
+        doctoreselimjson.close();
+    }
+    bool encontrar = false;
+    for (auto enc = doctores.begin(); enc != doctores.end();
+        ++enc) {
+        if ((*enc)["ID"] == idd) {
+            doctoreselim.push_back(*enc);
+            enc = doctores.erase(enc);
+            cout << "Doctor con el id:" << idd << "se ha eliminado" << endl;
+            encontrar = true;
+            break;
+        }
+    }
+    if (!encontrar) {
+        cout << "Error. doctor" << idd << "no encontrado" << endl;
+    }
+    ofstream cambio("doctores.json");
+    if (!cambio.is_open()) {
+        cout << "Error1" << endl;
+    }
+    cambio << doctores.dump(4);
+    cambio.close();
+    ofstream cambioelim("doctoreselim.json");
+    if (!cambioelim.is_open()) {
+        cout << "Error al guardar eliminado" << endl;
+    }
+    cambioelim << doctoreselim.dump(4);
+    cambioelim.close();
 }
-
+void deliminar() {
+    int idd;
+    cout << "Cual es el id del doctor que quieres eliminar?" << endl;
+    cin >> idd;
+    doctor::eliminardoctor(idd);
+}
 void EliminarD() {
 	int vol;
 	cout << "----------------------------" << endl;
